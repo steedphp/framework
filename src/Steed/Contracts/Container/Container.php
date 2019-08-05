ddf4262e0c99fb6a1e4cdd178e61a435c713a3fd
loouss <loouss@qq.com>
2019-08-05 23:19:49 +0800

    container interface

---
+++ src/Steed/Contracts/Container/Container.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Steed\Contracts\Container;
+
+use Psr\Container\ContainerInterface;
+
+interface Container extends ContainerInterface
+{
+
+    /**
+     * Register a shared binding in the container.
+     *
+     * @param string $abstract
+     * @param \Closure|string|null $concrete
+     * @return void
+     */
+    public function singleton($abstract, $concrete = null);
+
+    /**
+     * Register an existing instance as shared in the container.
+     *
+     * @param  string  $abstract
+     * @param  mixed   $instance
+     * @return mixed
+     */
+    public function instance($abstract, $instance);
+    /**
+     * Clear container instance
+     * @return mixed
+     */
+    public function flush();
+
+    /**
+     * getStats
+     * @return array
+     */
+    public function getStats(): array;
+
+    /**
+     * Get all $id
+     * @return array
+     */
+    public function getNames(): array;
+}
