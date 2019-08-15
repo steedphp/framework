9f2248043afa9e5210a13f53de197bd54c59c856
loouss <loouss@qq.com>
2019-08-15 16:53:56 +0800

    application

--- src/Steed/Foundation/Application.php
+++ src/Steed/Foundation/Application.php
@@ -7,4 +7,23 @@ use Steed\Contracts\Foundation\Application as ApplicationContract;
 class Application implements ApplicationContract
 {
 
+    /**
+     * The Steed framework version.
+     *
+     * @var string
+     */
+    const VERSION = '0.0.1';
+
+
+    public function __construct()
+    {
+    }
+
+
+    protected function initialize()
+    {
+
+    }
+
+
 }
