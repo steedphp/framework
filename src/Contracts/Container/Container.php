3958157b7d11d2d10787769856e5dd29885b0d28
loouss <loouss@qq.com>
2020-05-11 22:11:48 +0800

    psr

--- src/Contracts/Container/Container.php
+++ src/Contracts/Container/Container.php
@@ -7,35 +7,5 @@ use Psr\Container\ContainerInterface;
 interface Container extends ContainerInterface
 {
 
-    /**
-     * Get container instance
-     *
-     * @return mixed
-     */
-    public static function getInstance(): Container;
-
-    /**
-     * Get all $id
-     * @return array
-     */
-    public function getNames(): array;
-
-    /**
-     * 创建实例
-     *
-     * @param $abstract
-     * @param $parameters
-     * @return mixed
-     */
-    public function make(string $abstract, array $parameters);
-
-    /**
-     * 构建实例
-     *
-     * @param $concrete
-     * @return mixed
-     */
-    public function build($concrete);
-
 
 }
--- src/Contracts/Foundation/Application.php
+++ src/Contracts/Foundation/Application.php
@@ -5,4 +5,6 @@ namespace Steed\Contracts\Foundation;
 interface Application
 {
 
+
+
 }
