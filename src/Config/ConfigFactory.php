73c88a7e0cd6f5915f9f42691a56aa65639a4bf3
loouss <loouss@qq.com>
2020-05-14 20:44:08 +0800

    config factory

---
+++ src/Config/ConfigFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Steed\Framework\Config;
+
+
+use Steed\Framework\Contracts\Config\Config;
+
+class ConfigFactory
+{
+
+    public function __invoke() : Config
+    {
+
+        //TODO lode config file
+
+
+
+
+
+    }
+
+}
